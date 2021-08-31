405480156909003ab6955aa121207ba5f8fd288b
Lopunnii <kimi7@xtra.co.nz>
2021-08-31 14:32:34 +0900

    Added switch statement to playRound function

--- rps.js
+++ rps.js
@@ -1,18 +1,42 @@
 const options = [
-    "Rock",
-    "Paper",
-    "Scissors"
+    "rock",
+    "paper",
+    "scissors"
 ];
 
  computerPlay = options[Math.floor(Math.random() * options.length)]; 
 
- console.log(computerPlay) 
-
  function playRound (playerSelection, computerSelection) {
-
+     switch (true){
+              //Player Ties:
+         case playerSelection == computerSelection:
+              console.log("It's a tie!")
+              break;
+              // Player Wins:
+         case playerSelection == "rock" && computerSelection == "scissors":
+              console.log("You win! Rock beats Scissors!")
+              break;
+         case playerSelection == "paper" && computerSelection == "rock":
+              console.log("You win! Paper beats Rock!")
+              break;
+         case playerSelection == "scissors" && computerSelection == "paper":
+              console.log("You win! Scissors beats Paper!")
+              break;
+              // Player Loses:
+         case playerSelection == "rock" && computerSelection == "paper":
+              console.log("You lose! Paper beats Rock!")
+              break;
+         case playerSelection == "paper" && computerSelection == "scissors":
+              console.log("You lose! Scissors beats Paper!")
+              break;
+         case playerSelection == "scissors" && computerSelection == "rock":
+              console.log("You lose! Rock beats Scissors!")
+              break;
+     }
+    
  }
 
- computerSelection = computerPlay 
- playerSelection = ""
+ computerSelection = computerPlay; 
+ playerSelection = "rock";
 
  console.log(computerSelection)
