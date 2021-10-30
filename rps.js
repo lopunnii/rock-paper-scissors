c6d06854f0cf2829b7db3a3bed4919be829bbcd6
Lopunnii <kimi7@xtra.co.nz>
2021-10-30 22:32:08 +0900

    .

--- rps.js
+++ rps.js
@@ -8,7 +8,14 @@ function computerPlay () {
       return computerPlayResult; 
  }
 
+ computerSelection = computerPlay();
+
+ playerSelection = "rock";
+
  function playRound (playerSelection, computerSelection) {
+      console.log(playerSelection);
+      computerSelection = computerPlay();
+      console.log(computerSelection);
       let result ;
      switch (true){
          case playerSelection.toLowerCase() == computerSelection:
@@ -38,71 +45,9 @@ function computerPlay () {
     return result;
  }
 
-/* If statement version: 
-function playRound (playerSelection, computerSelection) {
-     let result; 
-     if (playerSelection.toLowerCase() == computerSelection) {
-          result = ("It's a tie!");
-} else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
-          result = ("You win! Rock beats Scissors!");
-} else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
-          result = ("You win! Paper beats Rock!");
-} else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
-          result = ("You win! Scissors beats Paper!");
-} else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
-          result = ("You lose! Paper beats Rock!");
-} else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
-          result = ("You lose! Scissors beats Paper!");
-} else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
-           result = ("You lose! Rock beats Scissors!")
-} else {
-          result = ("Please select rock, paper or scissors.");
-}
-   return result; 
-}
-*/
-
-/*function game() {
-     playRound(playerSelection, computerSelection);
- counter++;
-}
-*/
-  
- playerSelection = "rock";
- computerSelection = computerPlay(); 
-
- console.log(computerSelection)
- console.log(playerSelection)
+ console.log(playRound(playerSelection, computerSelection));
+ console.log(playRound(playerSelection, computerSelection));
+ console.log(playRound(playerSelection, computerSelection));
+ console.log(playRound(playerSelection, computerSelection));
  console.log(playRound(playerSelection, computerSelection));
 
- computerSelection = computerPlay(); 
- console.log(computerSelection)
- console.log(playerSelection)
- console.log(playRound(playerSelection, computerSelection));
-
-computerSelection = computerPlay(); 
- console.log(computerSelection)
- console.log(playerSelection)
- console.log(playRound(playerSelection, computerSelection));
-
- computerSelection = computerPlay(); 
- console.log(computerSelection)
- console.log(playerSelection)
- console.log(playRound(playerSelection, computerSelection));
-
- computerSelection = computerPlay(); 
- console.log(computerSelection)
- console.log(playerSelection)
- console.log(playRound(playerSelection, computerSelection));
-
- /*
-counter = 0;
-while (counter<5)
-{
-     computerSelection = computerPlay(); 
-     console.log(computerSelection)
-     console.log(playerSelection)
-     console.log(playRound(playerSelection, computerSelection));
-    counter ++;
-}
-*/ 
